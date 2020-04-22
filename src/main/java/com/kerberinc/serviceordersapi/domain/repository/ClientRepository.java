5b9f2a1ee9b8af64b222839df6083dc9a0b02f56
Adriano Kerber <kerberpro@gmail.com>
2020-04-22 17:02:43 -0300

    Add new methods for repository - the methods are implemented via DI of the JPA

--- src/main/java/com/kerberinc/serviceordersapi/domain/repository/ClientRepository.java
+++ src/main/java/com/kerberinc/serviceordersapi/domain/repository/ClientRepository.java
@@ -1,5 +1,7 @@
 package com.kerberinc.serviceordersapi.domain.repository;
 
+import java.util.List;
+
 import com.kerberinc.serviceordersapi.domain.model.Client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +9,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    List<Client> findByName(String name);
+    List<Client> findByNameContaining(String name);
 
 }
