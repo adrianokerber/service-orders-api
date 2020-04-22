c3746fd93d123d917c793c4feb19f8738e4080d8
Adriano Kerber <kerberpro@gmail.com>
2020-04-22 17:17:01 -0300

    Add requestMapping to map all endpoints of the controller to use the same base path - /clients

--- src/main/java/com/kerberinc/serviceordersapi/api/controller/ClientController.java
+++ src/main/java/com/kerberinc/serviceordersapi/api/controller/ClientController.java
@@ -10,20 +10,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
 	@Autowired
 	private ClientRepository clientRepository;
 
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> index() {
 		return clientRepository.findAll();
 	}
 	
-	@GetMapping("/clients/{clientId}")
+	@GetMapping("/{clientId}")
 	public ResponseEntity<Client> getClient(@PathVariable Long clientId) {
 		Optional<Client> client = clientRepository.findById(clientId);
 
