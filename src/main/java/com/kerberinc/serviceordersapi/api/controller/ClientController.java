a9ed7757189895a0cc8bcbfefc039ecde8339522
Adriano Kerber <kerberpro@gmail.com>
2020-04-22 17:47:23 -0300

    Add CRUD methods for Client table

--- src/main/java/com/kerberinc/serviceordersapi/api/controller/ClientController.java
+++ src/main/java/com/kerberinc/serviceordersapi/api/controller/ClientController.java
@@ -7,10 +7,16 @@ import com.kerberinc.serviceordersapi.domain.model.Client;
 import com.kerberinc.serviceordersapi.domain.repository.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,6 +40,42 @@ public class ClientController {
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client add(@RequestBody Client client) {
+		return clientRepository.save(client);
+	}
+
+	@PutMapping("/{clientId}")
+	public ResponseEntity<Client> update(
+		@PathVariable Long clientId,
+		@RequestBody Client client
+	) {
+		
+		if (!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		client.setId(clientId);
+		client = clientRepository.save(client);
+
+		return ResponseEntity.ok(client);
+
+	}
+
+	@DeleteMapping("/{clientId}")
+	public ResponseEntity<Void> delete(@PathVariable Long clientId) {
+
+		if (!clientRepository.existsById(clientId)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		clientRepository.deleteById(clientId);
+
+		return ResponseEntity.noContent().build();
+
 	}
 
 }
