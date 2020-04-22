2b9602b6831bbc30de2e9ebec8f8e45a091fcb6e
Adriano Kerber <kerberpro@gmail.com>
2020-04-22 17:15:03 -0300

    Add new endpoint to return single client or error using ResponseEntity

--- src/main/java/com/kerberinc/serviceordersapi/api/controller/ClientController.java
+++ src/main/java/com/kerberinc/serviceordersapi/api/controller/ClientController.java
@@ -1,12 +1,15 @@
 package com.kerberinc.serviceordersapi.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.kerberinc.serviceordersapi.domain.model.Client;
 import com.kerberinc.serviceordersapi.domain.repository.ClientRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +21,17 @@ public class ClientController {
     @GetMapping("/clients")
     public List<Client> index() {
 		return clientRepository.findAll();
-    }
+	}
+	
+	@GetMapping("/clients/{clientId}")
+	public ResponseEntity<Client> getClient(@PathVariable Long clientId) {
+		Optional<Client> client = clientRepository.findById(clientId);
+
+		if (client.isPresent()) {
+			return ResponseEntity.ok(client.get());
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 
 }
