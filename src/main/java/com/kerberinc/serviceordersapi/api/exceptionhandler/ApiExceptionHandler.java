954b21fb602eb94d6b37ab640aa0cb744231ebad
Adriano Kerber <kerberpro@gmail.com>
2020-04-22 19:13:35 -0300

    Add error handler with custom messages to DB errors

---
+++ src/main/java/com/kerberinc/serviceordersapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.kerberinc.serviceordersapi.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        var fields = new ArrayList<Issue.Field>();
+
+        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
+            String name = ((FieldError) error).getField();
+            String message = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+
+            fields.add(new Issue.Field(name, message));
+        }
+        
+        var issue = new Issue();
+        issue.setStatus(status.value());
+        issue.setTitle("Um ou mais campos estão inválidos." + " Faça o preenchimento correto e tente novamente");
+        issue.setDate(LocalDateTime.now());
+        issue.setFields(fields);
+        
+        return super.handleExceptionInternal(ex, issue, headers, status, request);
+    }
+
+}
---
+++ src/main/java/com/kerberinc/serviceordersapi/api/exceptionhandler/Issue.java
@@ -0,0 +1,77 @@
+package com.kerberinc.serviceordersapi.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Issue {
+
+    private Integer status;
+    private LocalDateTime date;
+    private String title;
+    private List<Field> fields;
+
+    public static class Field {
+
+        private String name;
+        private String message;
+
+        public Field(
+            String name,
+            String message
+        ) {
+            super();
+            setName(name);
+            setMessage(message);
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDate() {
+        return this.date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Field> getFields() {
+        return fields;
+    }
+
+    public void setFields(List<Field> fields) {
+        this.fields = fields;
+    }
+
+}
